1a9f4de75824cc496c7b4797a63a81e1f01a3d87
JonaHurt <jonhrtb@gmail.com>
2021-10-03 23:15:43 -0300

    completo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Home/>
+    <ul>
+      <li>lista 1</li>
+      <li>lista 2</li>
+      <li>lista 3</li>
+      <li>lista 4</li>
+    </ul>
+    </>
   );
 }
 
